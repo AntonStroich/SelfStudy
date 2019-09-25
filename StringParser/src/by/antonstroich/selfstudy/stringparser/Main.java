3b067dcf571662d50312c15871d8b24569332a88
AntonStroich <45979062+AntonStroich@users.noreply.github.com>
2019-09-25 12:01:34 +0300

    Add files via upload

---
+++ StringParser/src/by/antonstroich/selfstudy/stringparser/Main.java
@@ -0,0 +1,23 @@
+package by.antonstroich.selfstudy.stringparser;
+
+import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try {
+			String pathToGetFile = "c:/myFiles/String.txt";
+			ReadingOfFile myReadingOfFile = new ReadingOfFile();
+			String fileContent = myReadingOfFile.readFile(pathToGetFile);
+			Parcer myParcer = new Parcer();
+			List<String> newStrings = myParcer.separateFileContentToStrings(fileContent);
+			Printer myPrinter = new Printer();
+			myPrinter.printFileContent(newStrings);
+		} catch (Exception e) {
+			System.out.println("Program can't be finished!");
+			e.printStackTrace();
+		}
+	}
+}
---
+++ StringParser/src/by/antonstroich/selfstudy/stringparser/Parcer.java
@@ -0,0 +1,15 @@
+package by.antonstroich.selfstudy.stringparser;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Parcer {
+
+	public List<String> separateFileContentToStrings(String fileContent) {
+		List<String> stringsAfterParsing = new ArrayList();
+		for (String newString : fileContent.split(";")) {
+			stringsAfterParsing.add(newString);
+		}
+		return stringsAfterParsing;
+	}
+}
